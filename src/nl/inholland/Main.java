3d1f4d812a2a83cce06802f424087302676edbf6
Sander Harks <650397@student.inholland.nl>
2021-09-12 15:24:41 +0200

    rename

--- src/nl/inholland/Main.java
+++
@@ -1,8 +0,0 @@
-package nl.inholland;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
---
+++ src/nl/inholland/Program.java
@@ -0,0 +1,13 @@
+package nl.inholland;
+
+public class Program {
+
+    public static void main(String[] args) {
+	    Program p = new Program();
+        p.start();
+    }
+
+    void start() {
+
+    }
+}
